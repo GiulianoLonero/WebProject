120b499a1fc4b2a5339e90da6eb7f1a21325ec80
manunow04 <e.adesso2@studenti.poliba.it>
2026-05-11 19:27:08 +0200

    authentication adjustements

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,10 +3,23 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt")
 const User = require("../models/User");
+require("dotenv").config();
+const rateLimiter = require("express-rate-limit")
 
-router.post("/", async(req,res) => {
+const loginLimiter = rateLimit({
+    windowsMs: 5*60*1000,
+    max: 3,
+    message: {
+        message: "Too many trials, try again in 5 minutes"
+    },
+    standardHeaders: true //forse da togliere
+})
+
+router.post("/", loginLimiter, async(req,res) => {
     try{
         const {mail, password} = req.body;
+        if (typeof(mail) !== "string" || typeof(password) !== "string")
+            return res.status(400).json({message: "Invalid input type"})
 
         const user = await User.findOne({mail: mail});
         
@@ -20,13 +33,14 @@ router.post("/", async(req,res) => {
             return res.status(400).json({message: "Email or Password not correct!"});
         }
 
-        const token = jwt.sign({id: user._id}, "Francesco Marzano", {expiresIn: "1h"});
+        const token = jwt.sign({id: user._id}, {secretOrPrivateKey: process.env.privateKey}, {expiresIn: "1h"});
 
         res.status(200).json({message: "Successfully logged in!", token: token})
         
     } catch(error){
 
-        res.status(500).json({message: "Server error", error: error.message})
+        res.status(500).json({message: "Server error"})
+        console.log(error.message)
     }
 });
 
