2a4814f5160f430be9740363b4fe0e28ff8fd16d
manunow04 <e.adesso2@studenti.poliba.it>
2026-05-27 18:31:48 +0200

    LoginForm.jsx added

--- frontend/src/Components/LoginForm/LoginForm.jsx
+++ frontend/src/Components/LoginForm/LoginForm.jsx
@@ -1,17 +1,60 @@
-import React from "react";
-import "./LoginForm.css"
+import React, { useState } from 'react';
+import "./LoginForm.css";
+import axios from "axios";
 
-const LoginForm = () =>{
+
+function InputBox({type, placeholder, value, onChange}) {
+    return (
+        <div classname="input-box">
+            <input
+                type = {type}
+                placeholder = {placeholder}
+                value = {value}
+                onChange = {onChange}
+                required
+            />
+        </div>
+    );  
+}
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrorMessage("");
+    try {
+        const response = await axios.post("http://localhost:5000/api/auth",
+            {
+                mail: email,
+                password: password
+            },
+            {
+                withCredentials: true
+            }
+        );
+        const {accessToken} = response.data;
+    } catch(error){
+        if (error.response)
+            setErrorMessage(error.response.data.message)
+    }
+}
+const LoginForm = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
     return (
         <div className="wrapper">
-            <form action="">
+            <form className="form" onSubmit = {handleSubmit}>
                 <h1> Login </h1>
-                <div className="input-box">
-                    <input type="text" placeholder="mail" required />
-                </div>
-                <div className="input-box">
-                    <input type="password" placeholder="Password" required />
-                </div>
+                <InputBox 
+                type = "text"
+                placeholder = "mail"
+                value = {email}
+                onChange={e => setEmail(e.target.value)}
+                />
+                <InputBox 
+                type = "text"
+                placeholder = "password"
+                value = {password}
+                onChange={e => setPassword(e.target.value)}
+                />
                 <button type="submit">Login</button>
             </form>
         </div>
