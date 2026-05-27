487e5177142d71d7cd37b9370ec8452ef14e4139
GiulianoLonero <g.lonero2@studenti.poliba.it>
2026-05-27 16:48:55 +0200

    Added frontend

---
+++ frontend/src/Components/LoginForm/LoginForm.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./LoginForm.css"
+
+const LoginForm = () =>{
+    return (
+        <div className="wrapper">
+            <form action="">
+                <h1> Login </h1>
+                <div className="input-box">
+                    <input type="text" placeholder="mail" required />
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Password" required />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+export default LoginForm;
