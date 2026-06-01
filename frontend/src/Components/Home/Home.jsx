05c735f2b9def13a7ea9de201b094922fb2cb861
manunow04 <e.adesso2@studenti.poliba.it>
2026-06-01 19:30:14 +0200

    Finished Navbar and added parameters to Home

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAuth } from "../../hooks/useAuth";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import Navbar from "../Navbar/Navbar"
+import { useState } from "react"
+import axios from "axios"
 
 const Home = function(){
     const { user, isAuth} = useAuth();
     const navigate = useNavigate();
 
+    const [searchParams] = useSearchParams();
+    const [events, setEvents] = useState([]) //qui metteremo tutti gli eventi
+
+    useEffect(() => {
+        const filteredEvents = async () => {
+            try {
+                const genre = searchParams.get("genre")
+                const location = searchParams.get("location")
+                const search = searchParams.get("search")
+
+                const response = await axios.get("http://localhost:5000/api/events", {
+                    params: {
+                        genre: genre,
+                        location: location,
+                        search: search
+                    }
+                });
+                setEvents(response.data)
+            } catch (error){
+                console.error("Error during events research", error.message)
+            }
+        }
+    });
+
     return( 
         <div>
+            <Navbar />
             {isAuth ? (
                 <div>
                     <h1>Pagina di utente loggato</h1>
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -4,25 +4,37 @@ import styles from "./Navbar.module.css"
 
 
 const Navbar = function(){
+    const navigate = useNavigate()
+    const [search, setSearch] = useState("")
+    const [genre, setGenre] = useState("Genere")
+    const [location, setLocation] = useState("Luogo")
     return(
         <div className={styles.Navbar}>
-            <form className={styles.form}>
-                <select name="genre" className={styles.select}>
+            <form className={styles.form} onSubmit = {(e) =>
+                {e.preventDefault()
+                navigate(`/?genre=${genre}&location=${location}&search=${search}`)}}>
+                <select name="genre" placeholder="Genere" className={styles.select} value={genre} onChange={(e)=>setGenre(e.target.value)}>
                     <option value="music">Concerto</option>
                     <option value="cinema">Cinema</option>
                     <option value="theatre">Teatro</option>
                 </select>
-                <select name="location" className={styles.select}>
+                <select name="location" placeholder="Luogo" className={styles.select} value={location} onChange={(e)=>setLocation(e.target.value)}>
                     <option value="bari">Bari</option>
                     <option value="roma">Roma</option>
                     <option value="taranto">Taranto</option>
                     <option value="torino">Torino</option>
                     <option value="catanzaro">Catanzaro</option>
                 </select>
-                <input type="text" className={styles.searchBar} placeholder="Ricerca qui..."></input>
+                <input 
+                type="text" 
+                className={styles.searchBar} 
+                placeholder="Ricerca qui..." 
+                value={search} 
+                onChange = {(e)=>setSearch(e.target.value)} ></input>
                 <button type="submit" className={styles.button}>Cerca</button>
             </form>
         </div>
     )
 }
+export default Navbar;
 
